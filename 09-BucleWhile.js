defe70178f6bd6221ef3bd151afb5cf493eb444f
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-01-20 22:40:48 -0500

    He aprendido el uso de un While, ejecuta siempre uy cuando se de dicha condición.

---
+++ 09-BucleWhile.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// BUCLE WHILE
+// COMPROBAMOS UNA CONDICIÓN Y LA EJECUTAMOS
+
+var year = 2018;
+
+while (year <= 2051) {
+    console.log("Estamos en el año: " + year);
+    year++;
+}
