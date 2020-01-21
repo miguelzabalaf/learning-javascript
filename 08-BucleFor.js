9d828eaa31eceb8ed5fe25a13029881c7df1fdb8
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-01-20 22:33:37 -0500

    He aprendido y dominado la estructura del ciclo for y sus diferentes funciones en JavaScript

---
+++ 08-BucleFor.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// BUCLE FOR
+// BUCLE ES UNA ESTRUCTURA DE CONTROL QUE SE REPITE VARIAS VECES.
+
+var numero = 100;
+
+// CONTADOR - CONDICIÓN - INCREMENTO
+for (var i = 1; i <= numero; i++) {
+    console.log('Vamos por el número: ' + i);
+}
